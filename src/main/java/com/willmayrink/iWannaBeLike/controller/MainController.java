ebc36f5aa4738c9c907e693ac9125688edd8b8f4
Will Mayrink <will.mayrink@gmail.com>
2025-09-24 12:06:37 -0300

    Add login page and update UI for role model view
    Introduces login.html and templateLogin.html for user authentication, adds related background images, and updates MainController to route to the new login templates. Enhances style.css with login-specific styles and refines the radar chart in viewRoleModel.html to normalize measurements and improve layout.

--- src/main/java/com/willmayrink/iWannaBeLike/controller/MainController.java
+++ src/main/java/com/willmayrink/iWannaBeLike/controller/MainController.java
@@ -4,6 +4,7 @@ import com.willmayrink.iWannaBeLike.repository.rolemodel.RoleModelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,8 +14,16 @@ public class MainController {
     RoleModelRepository roleModelRepository;
 
     @RequestMapping("/")
-    public String listAllModels(Model model){
+    public String listAllModels(){
+        return "templateLogin";
+    }
+    @GetMapping("/role_models")
+    public String firstPage(Model model){
         model.addAttribute("role_model", roleModelRepository.findAll());
         return "listAll";
+    }
+    @GetMapping("/login-custom")
+    public String viewLoginPage(){
+        return  "templateLogin";
     }
 }
